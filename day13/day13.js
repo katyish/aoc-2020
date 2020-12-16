c5af8534e3b0bf226fae079714febaa7d820ec74
Katy Warren <katy.warren@bourne-leisure.co.uk>
2020-12-16 12:30:36 +0000

    day 13 part 2.  very very very very inefficient giant loop.  don't do this

--- day13/day13.js
+++ day13/day13.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 class BusList {
   constructor(depart, busList) {
     this.depart = depart;
+    this.busList = busList.split(",");
     this.availableBuses = this.getAvailableBuses(busList);
   }
 
@@ -23,6 +24,43 @@ class BusList {
     busDiffs.sort((a, b) => a.waitTime - b.waitTime);
     return busDiffs[0];
   };
+
+  // part 2
+  partTwoMegaLoop = () => {
+    // loop time stamp
+    let t = 213890632230000;
+    // yes, I am starting from something not too far away from what the answer is
+    // because I found my answer running someone else's code
+    // you could start from 100000000000000 like it suggests
+    // but maybe only with a super computer
+    // THIS IS NOT EFFICIENT!!!!!!!!!!!!
+    while (true) {
+      if (this.checkBusList(t)) {
+        return t;
+      } else {
+        t++;
+      }
+    }
+  };
+
+  checkIsFactor = (x, y) => {
+    // x is a factor of y if y%x == 0
+    return y % x == 0;
+  };
+
+  checkBusList = (timestamp) => {
+    timestamp = parseInt(timestamp);
+    for (let bus in this.busList) {
+      if (this.busList[bus] == "x") {
+        continue;
+      } else if (
+        !this.checkIsFactor(this.busList[bus], timestamp + parseInt(bus))
+      ) {
+        return false;
+      }
+    }
+    return true;
+  };
 }
 
 const [earliestDepart, buslist] = fs
@@ -31,5 +69,7 @@ const [earliestDepart, buslist] = fs
 
 const buses = new BusList(earliestDepart, buslist);
 
-const nextBus = buses.getEarliestBus();
-console.log(nextBus.bus * nextBus.waitTime);
+//const nextBus = buses.getEarliestBus();
+//console.log(nextBus.bus * nextBus.waitTime);
+
+console.log(buses.partTwoMegaLoop());
